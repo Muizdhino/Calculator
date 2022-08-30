d947068636280798ced856eea01f0e6747486029
Muiz Ayomide <muizayomide2@gmail.com>
2022-08-30 16:50:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+const display = document.getElementById('display');
+const numberZero = document.getElementById('0');
+const numberOne = document.getElementById('1');
+const numberTwo = document.getElementById('2');
+const numberThree = document.getElementById('3');
+const numberFour = document.getElementById('4');
+const numberFive = document.getElementById('5');
+const numberSix = document.getElementById('6');
+const numberSeven = document.getElementById('7');
+const numberEight = document.getElementById('8');
+const numberNine = document.getElementById('9');
+const delValue = document.getElementById('del')
+
+const clearValue = document.getElementById('C')
+
+function addValue(event) {
+    if (display.innerText == "0") {
+        display.innerText = event.target.value;
+    }
+    else {
+        display.innerText += event.target.value;
+    }
+
+
+}
+function clearValueFunc(event) {
+    display.innerText = "0";
+}
+
+function delValueFunc(event) {
+    let result = display.innerText;
+
+    if (result.length === 1) {
+        display.innerText = "0";
+    }
+    else {
+        display.innerText = result.substring(0, result.length - 1);
+    }
+
+
+}
+
+clearValue.addEventListener('click', clearValueFunc)
+numberZero.addEventListener('click', addValue)
+numberOne.addEventListener('click', addValue)
+numberTwo.addEventListener('click', addValue)
+numberThree.addEventListener('click', addValue)
+numberFour.addEventListener('click', addValue)
+numberFive.addEventListener('click', addValue)
+numberSix.addEventListener('click', addValue)
+numberSeven.addEventListener('click', addValue)
+numberEight.addEventListener('click', addValue)
+numberNine.addEventListener('click', addValue)
+delValue.addEventListener('click', delValueFunc)
+
